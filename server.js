35196ac65a89123ed7351861fddf82162515b678
Jason-bolt <appiatujason@gmail.com>
2022-12-13 14:29:32 +0000

    Completed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 const db = require("./config/db");
-db.sequelize.sync({ force: true })
+db.sequelize.sync()
   .then(() => {
     console.log("Synced db.");
   })
