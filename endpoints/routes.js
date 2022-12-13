35301cdafe9dffac4cffad12e5d91e46790dffd2
Jason-bolt <appiatujason@gmail.com>
2022-12-13 13:46:19 +0000

    Added initial route

--- endpoints/routes.js
+++ endpoints/routes.js
@@ -2,6 +2,11 @@ const router = require('express').Router()
 const Incident_Middleware = require('../middleware/incident_middleware')
 const Incident_Controller = require('../contoller/incident_controller')
 
+router.get('/', (req, res) => {
+    res.send(
+        "/incidents ... to view incidents <br /> /report to make reports"
+    )
+})
 
 router.get('/incidents', Incident_Controller.getIncidents)
 
